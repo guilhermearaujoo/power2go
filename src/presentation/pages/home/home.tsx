bccfef93da1b28ea2d9679d2eb2a5ca535fbc01d
guilhermearaujoo <ghma.em1@gmail.com>
2024-03-07 12:10:34 -0300

    chore: start project with initial config

---
+++ src/presentation/pages/home/home.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Styles from './home-styles.scss'
+
+const Home: React.FC = () => {
+  return <div className={Styles.home}>
+    <h1>Hello World</h1>
+  </div>
+}
+
+export default Home

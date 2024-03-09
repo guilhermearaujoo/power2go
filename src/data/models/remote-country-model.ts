f7661d50088c40d3f568377fc56f6426ee18fa6c
guilhermearaujoo <ghma.em1@gmail.com>
2024-03-09 20:24:03 -0300

    feat: create RemoteCountryModel

---
+++ src/data/models/remote-country-model.ts
@@ -0,0 +1,13 @@
+
+export type RemoteCountryModel = {
+  flags: {
+    svg: string
+  }
+  name: {
+    common: string
+  }
+  currencies: object
+  capital: string[]
+  languages: object
+  population: number
+}

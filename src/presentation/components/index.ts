24fc07cb854675715ef95a5be7d1df21433548c1
guilhermearaujoo <ghma.em1@gmail.com>
2024-03-11 18:11:47 -0300

    refactor: add Spinner and Loading to default export file

--- src/presentation/components/index.ts
+++ src/presentation/components/index.ts
@@ -1,2 +1,4 @@
 export { default as Table } from './table/table'
 export { default as Accordion } from './accordion/accordion'
+export { default as Loading } from './loading/loading'
+export { default as Spinner } from './spinner/spinner'

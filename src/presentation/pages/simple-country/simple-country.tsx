184fe49d24f15d500d986e9efb205965d484a86f
guilhermearaujoo <ghma.em1@gmail.com>
2024-03-11 18:14:14 -0300

    feat: shows data based on url in Simple CountryComponent

--- src/presentation/pages/simple-country/simple-country.tsx
+++ src/presentation/pages/simple-country/simple-country.tsx
@@ -1,21 +1,56 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useCallback } from 'react'
 import Styles from './simple-country-styles.scss'
 import { useLocation } from 'react-router-dom'
 import { LoadCountry } from '@/domain/usecases'
+import { Loading, Table, Accordion } from '@/presentation/components'
+import { CountryModel } from '@/domain/models'
 
 type Props = {
   loadCountry: LoadCountry
 }
 
 const SimpleCountry: React.FC<Props> = ({ loadCountry }: Props) => {
+  const [isLoading, setIsLoading] = React.useState(true)
+  const [country, setCountry] = React.useState<CountryModel>(null)
+  const { pathname } = useLocation()
+
+  const loadCountryByName = useCallback(
+    (country) => {
+      loadCountry
+        .load(country)
+        .then((countryData) => setCountry(countryData))
+        .catch(console.error)
+        .finally(() => setIsLoading(false))
+    },
+    [pathname]
+  )
+
   useEffect(() => {
-    loadCountry.load('brasil').then(countries => console.log(countries)).catch(console.error)
+    setIsLoading(true)
+    loadCountryByName(pathname.split('/')[1])
   }, [])
 
-  const { pathname } = useLocation()
-  return <div className={Styles.simpleCountry}>
-        {pathname.split('/')[1]}
-  </div>
+  return (
+    <div className={Styles.simpleCountry}>
+      {isLoading && <Loading />}
+      {country && (
+        <Accordion title={`Showing ${country.name} information`}>
+          <Table
+            headers={['Name', 'Capital', 'Currency', 'Language', 'Population']}
+            values={[
+              [
+                country.name,
+                country.capital,
+                country.currency,
+                country.population,
+                country.population
+              ]
+            ]}
+          />
+        </Accordion>
+      )}
+    </div>
+  )
 }
 
 export default SimpleCountry

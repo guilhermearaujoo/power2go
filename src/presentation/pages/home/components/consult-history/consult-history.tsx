defc591619734935d9d78a9511b679e2b830bb79
guilhermearaujoo <ghma.em1@gmail.com>
2024-03-12 08:54:09 -0300

    feat: add function to dowload csv file from ConsultHistory

--- src/presentation/pages/home/components/consult-history/consult-history.tsx
+++ src/presentation/pages/home/components/consult-history/consult-history.tsx
@@ -1,23 +1,50 @@
-import React from 'react'
+import Styles from './consult-history-styles.scss'
 import { Table, Accordion } from '@/presentation/components'
 import { Context } from '@/main/factories/context/context'
 import { ConsultHistoryModel } from '@/domain/models'
+import CsvDownload from 'react-csv-downloader'
+
+import React, { useCallback } from 'react'
 
 const ConsultHistory: React.FC = () => {
   const { consultHistory } = React.useContext(Context)
 
+  const columns = [
+    { id: 'date', dispplayName: 'Date' },
+    { id: 'country', dispplayName: 'Country' },
+    { id: 'link', dispplayName: 'Link' }
+  ]
+
+  const datas = useCallback(() =>
+    consultHistory().map((consult: ConsultHistoryModel) => ({
+      date: consult?.date,
+      country: consult?.country,
+      link: consult?.link
+    })), [consultHistory])
+
   return (
     <Accordion title="Histórico de consultas">
       <Table
-        headers={['Data', 'País', 'Link']}
-        values={consultHistory().map((consult: ConsultHistoryModel, index: number) => [
-          consult?.date,
-          consult?.country,
-          <a href={consult?.link} key={index}>
-            {consult?.link}
-          </a>
-        ]).reverse()}
+        headers={columns.map((column) => column.dispplayName)}
+        values={datas()
+          .map((data: ConsultHistoryModel) => [
+            data.date,
+            data.country,
+            <a href={data.link} key={data.country}>
+              {data.link}
+            </a>
+          ])
+          .reverse()}
       />
+
+        <CsvDownload
+          extension=".csv"
+          filename={'consult-history_' + new Date().toLocaleDateString()}
+          columns={columns}
+          datas={datas}
+          text="Export CSV"
+          className={Styles.dowloadButton}
+        />
     </Accordion>
   )
 }

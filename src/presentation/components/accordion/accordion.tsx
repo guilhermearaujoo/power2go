ee8334d771c509139b4817adbfc80555d9d69df3
guilhermearaujoo <ghma.em1@gmail.com>
2024-03-12 21:23:37 -0300

    feat: allow Accordion to tecive more classes

--- src/presentation/components/accordion/accordion.tsx
+++ src/presentation/components/accordion/accordion.tsx
@@ -1,14 +1,15 @@
 import React, { memo } from 'react'
 import Styles from './accordion-styles.scss'
 
-interface AccordionProps {
+type AccordionProps = React.HTMLAttributes<HTMLElement> & {
   children: React.ReactNode
   title: string
+  aditionalClass?: string
 }
 
-const Accordion: React.FC<AccordionProps> = ({ children, title }) => {
+const Accordion: React.FC<AccordionProps> = ({ children, title, aditionalClass, ...props }) => {
   return (
-    <div className={Styles.accordion}>
+    <div className={`${Styles.accordion} ${aditionalClass}`} {...props}>
       {title && <div className={Styles.header}>{<h2>{title}</h2>}</div>}
       {children}
     </div>

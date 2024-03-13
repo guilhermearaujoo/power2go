da5240f03ad4dd1cc1a98dc05e94a4fd35cb5a9e
guilhermearaujoo <ghma.em1@gmail.com>
2024-03-12 21:35:46 -0300

    feat: add pagination and change button in ConsultHistory

--- src/presentation/pages/home/components/consult-history/consult-history.tsx
+++ src/presentation/pages/home/components/consult-history/consult-history.tsx
@@ -4,10 +4,14 @@ import { Context } from '@/main/factories/context/context'
 import { ConsultHistoryModel } from '@/domain/models'
 import CsvDownload from 'react-csv-downloader'
 
+import Pagination from '@mui/material/Pagination'
+import { BiCloudDownload } from 'react-icons/bi'
+
 import React, { useCallback } from 'react'
 
 const ConsultHistory: React.FC = () => {
   const { consultHistory } = React.useContext(Context)
+  const [page, setPage] = React.useState(1)
 
   const columns = [
     { id: 'date', dispplayName: 'Date' },
@@ -15,36 +19,61 @@ const ConsultHistory: React.FC = () => {
     { id: 'link', dispplayName: 'Link' }
   ]
 
-  const datas = useCallback(() =>
-    consultHistory().map((consult: ConsultHistoryModel) => ({
-      date: consult?.date,
-      country: consult?.country,
-      link: consult?.link
-    })), [consultHistory])
+  const datas = useCallback(
+    () =>
+      consultHistory()
+        .map((consult: ConsultHistoryModel) => ({
+          date: consult?.date,
+          country: consult?.country,
+          link: (
+            <a href={consult.link} key={consult.country}>
+              {consult.link}
+            </a>
+          )
+        }))
+        .reverse(),
+    [consultHistory]
+  )
+
+  const handleChange = (
+    event: React.ChangeEvent<unknown>,
+    value: number
+  ): void => {
+    setPage(value)
+  }
+  console.log(datas().length % 5)
 
   return (
-    <Accordion title="Consult History">
+    <Accordion title="Consult History" aditionalClass={Styles.accordion} >
       <Table
         headers={columns.map((column) => column.dispplayName)}
-        values={datas()
-          .map((data: ConsultHistoryModel) => [
-            data.date,
-            data.country,
-            <a href={data.link} key={data.country}>
-              {data.link}
-            </a>
-          ])
-          .reverse()}
+        values={new Array(5).fill(0).map((_, index) => {
+          const consult = datas()[index + (page - 1) * 5]
+          return consult ? [consult.date, consult.country, consult.link] : []
+        })}
       />
 
+      <div className={Styles.dowloadWrapper} title='Dowload CSV'>
         <CsvDownload
           extension=".csv"
           filename={'consult-history_' + new Date().toLocaleDateString()}
           columns={columns}
           datas={datas}
-          text="Export CSV"
+          text="."
           className={Styles.dowloadButton}
         />
+        <button className={Styles.downloadIcon}>
+          <BiCloudDownload />
+        </button>
+      </div>
+
+      <div className={Styles.pagination}>
+        <Pagination
+          count={Math.ceil(datas().length / 5) || 1}
+          page={page}
+          onChange={handleChange}
+        />
+      </div>
     </Accordion>
   )
 }
